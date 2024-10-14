8d70cce81805a7a0f8d2e61cba2c5e4b697e62b9
Tram <tramdang7907129@gmail.com>
2024-10-14 16:14:52 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+interseciton = set([1, 2]) & set([3, 4, 5])
+
+print(interseciton == set())
+
+print([1].index(max([1])))
+
+for i in range(1):
+    print(i)
+
+dict = dict()
+dict['2'] = 'hello'
+print(dict['2'])
